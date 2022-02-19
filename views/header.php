710233652e2ea2456d5c0a0dd01acf3ff8190bf9
Prabakaran <prabakaran271202@gmail.com>
2022-02-19 09:46:09 +0530

    navbar

--- views/header.php
+++ views/header.php
@@ -4,7 +4,53 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+   <?php include 'public/css/styles.css'?></style>
     <title>National Service Scheme</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="public/js/script.js"></script>
 </head>
 <body>
-    <header></header>
+    <header>
+    <div class="row" id="topnav">
+        <div class="col" id="logo" style="padding: 0px;">
+            <img  src="public\img\logo.png"> 
+        </div>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <i class="fa fa-bars" style="color:white"></i>
+  </a>
+        <div class="col-sm-4">
+            
+            </div>
+            <div class="col">
+                <p>
+            Home
+                </p>
+            </div>
+            <div class="col">
+                <p>
+            History</p>
+            </div>
+            <div class="col">
+                <p>
+            People</p>
+            </div>
+            <div class="col">
+                <p>
+            Awards</p>
+            </div>
+            <div class="col">
+                <p>
+            Activities</p>
+            </div>
+            
+            
+           
+          
+    </div>
+
+    </header>
+</body>
+</html>
