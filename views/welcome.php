2761f59304c7fe3ab456c818fceee42b87f2540f
prabakaran <prabakaran271202@gmail.com>
2022-03-13 19:21:55 +0530

    changed homescreen image and events

--- views/welcome.php
+++ views/welcome.php
@@ -1,18 +1,18 @@
-<div style="width:100%;;margin:0px;padding:0px;height:auto;background-color:white;" id="bgimg">
+<div style="width:100%;;margin:0px;padding:0px;height:auto;background-color:white;margin-top:-16px" id="bgimg">
   <div  class="container-fluid mt-3" style="margin:0px;padding:0px;">
-    <div class="col-sm-12" style="height:280px;padding-left:50px; background-image:url(public/img/3.jpg);float:none">
+    <div class="col-sm-12" style="height:500px;padding-left:50px; background-image:url(public/img/home.png);background-size:100% 500px;background-repeat:no-repeat;float:none">
       
-      <center>
-      <p style="font-size:25px;padding-top:45px;" id="msg">WELCOME</p>
-      <p style="font-size:25px;" id="msg">TO</p>
-      <p style="font-size:30px;color:white;">NATIONAL SERVICE SCHEME</p>
+      <center id="hometext" >
+      <p style="padding-top:55px;" id="msg">WELCOME</p>
+      <p style="" id="msg">TO</p>
+      <p style="">NATIONAL SERVICE SCHEME</p>
       </center>
   </div>
 </div>
   <div style="height:30px;">
   <div class="events" style="display: flex;">
   <div class="event-name" style="width: 100px; background-color:#303983">
-    <h2 style="text-align: center;font-size:25px;">Events</h2>
+    <h2 style="text-align: center;font-size:25px;color:white">Events</h2>
   </div>
   <marquee class="news-content" width="100%" direction="right" height="30px" style="background-color:white;">
   <center>
@@ -21,7 +21,7 @@
     $sql="select*from events";
     $result=mysqli_query($con,$sql);
   while($row=mysqli_fetch_assoc($result)){
-      echo "<span>".$row['e_name']."| </span>";
+      echo "<span>".$row['e_name']."</span><span style='margin-left:10px;margin-right:10px'>|</span>";
   }
 
 
